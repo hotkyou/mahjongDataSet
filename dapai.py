3d9c2156970d0b244048ca7129b485a2ddb0883d
hotkyou <hottarakasi.stu@gmail.com>
2024-05-07 16:05:49 +0900

    add: ミンカン判定処理

--- dapai.py
+++ dapai.py
@@ -1,4 +1,5 @@
 import csv
+import copy
 from collections import Counter
 
 def dapai(self, i):
@@ -8,7 +9,7 @@ def dapai(self, i):
     player = i["dapai"]["l"]
     tile = i["dapai"]["p"]
     tmp = ""
-    tehaitmp = self.tehaiok.copy()
+    tehaitmp = []
     
     if self.reach[player] != 1: #リーチしていない時
         if len(tile) >= 3:
@@ -69,61 +70,57 @@ def dapai(self, i):
         raise ValueError("手牌に存在しない")
     
     #手牌一時保存・手牌に捨て牌追加して鳴きチェック
-    #print(tehaitmp)
-    for tehaiplayer, tehainakami in enumerate(tehaitmp):
-        #print(tehaiplayer, tehainakami)
-        if tehaiplayer != player:   #捨て牌を捨てた本人は鳴けないのでスキップ
-            if self.reach[tehaiplayer] != 1: #立直してない人のみ
-                #tehaitmp[tehaiplayer][self.dorall.index(tmp)] += 1
-                print(tehaiplayer, tehaitmp[tehaiplayer])
-                
+    if self.todo != 0:
+        tehaitmp = copy.deepcopy(self.tehaiok) #現在の手牌に捨て牌を追加して鳴きをチェックするための変数
+        for tehaiplayer, tehainakami in enumerate(tehaitmp):
+            #print(tehaiplayer, tehainakami)
+            if tehaiplayer != player:   #捨て牌を捨てた本人は鳴けないのでスキップ
+                if self.reach[tehaiplayer] != 1: #立直してない人のみ
+                    tehaitmp[tehaiplayer][self.dorall.index(tmp)] += 1
+                    #print(tehaitmp[tehaiplayer])
 
-    if self.todo == 1: #ぽん
-        for n, o in enumerate(self.tehaiok):
-            #print(n, o)
-            if n != player:
-                pass
-    elif self.todo == 2: #チー
-        for n, o in enumerate(self.tehaiok):
-            #print(n, o)
-            if n != player:
-                pass
-    elif self.todo == 3: #カンをしたらgangで削除処理　全プレイヤーの手牌を処理
-        #print(player)
-        for n, o in enumerate(self.tehaiok):
-            #print(n, o)
-            if n != player: #鳴いた人以外のカンできるかチェック
-                if Counter(o)[3] >= 1:  # 明カン
-                    #print(Counter(o))
-                    #raise ValueError("カン3")
-                    data = []
-                    data += self.tehaiok[player] #手牌
-                    for j in range(len(self.reach)): #リーチ自分から見て
-                        index = (player + j) % len(self.reach)
-                        data.append(self.reach[index])
-                    data += self.dora #ドラ34
-                    data.append(self.parentdora) #場風
-                    data.append(self.childdora) #自風
-                    data.append(self.changbang) #何本場
-                    data.append(self.lizhibang) #リーチ棒繰越
-                    for k in range(len(self.naki)): #鳴き自分から見て
-                        index = (player + k) % len(self.naki)
-                        data.extend(self.naki[index])
-                    for l in range(len(self.discard)): #捨て牌自分から見て
-                        index = (player + l) % len(self.discard)
-                        data.extend(self.discard[index])
-                    for m in range(len(self.score)): #点数自分から見て
-                        index = (player + m) % len(self.score)
-                        data.append(self.score[index])
-                    data.append(self.tiles) #残り牌数
-                    data.append(0) #0が鳴きなし 1がカン
-                if Counter(o)[4] >= 1:  # 手牌に4枚持っているがカンしていない時
-                    pass
-            else:
-                if Counter(o)[4] >= 1:  # 暗カン
-                    #print(Counter(o))
-                    #print(self.naki[player])
-                    pass
-                    #raise ValueError("カン４")
-    else:
-        pass
+                    if self.todo == 1: #ぽん
+                        for n, o in enumerate(self.tehaiok):
+                            #print(n, o)
+                            if n != player:
+                                pass
+                    elif self.todo == 2: #チー
+                        for n, o in enumerate(self.tehaiok):
+                            #print(n, o)
+                            if n != player:
+                                pass
+                    elif self.todo == 3: #カンをしたらgangで削除処理　全プレイヤーの手牌を処理
+                        indexes = []
+                        for count, element in enumerate(tehainakami): #捨て牌と手牌を合わせてミンカンできるか確認
+                            if element == 4: #手牌と捨て牌を合わせて4枚ある時
+                                indexes.append(count)
+                        if indexes != []: #4枚ある時
+                            if self.dorall.index(tmp) in indexes: #捨て牌と手牌を合わせて4枚ある時の中に捨て牌がある時
+                                #print(indexes)
+                                #print(tehainakami)
+                                #print(self.dorall.index(tmp))
+                                print(self.tehaiok)
+                                data = []
+                                data += tehainakami #手牌
+                                for j in range(len(self.reach)): #リーチ自分から見て
+                                    index = (tehaiplayer + j) % len(self.reach)
+                                    print(index)
+                                    data.append(self.reach[index])
+                                data += self.dora #ドラ34
+                                data.append(self.parentdora) #場風
+                                data.append(self.childdora) #自風
+                                data.append(self.changbang) #何本場
+                                data.append(self.lizhibang) #リーチ棒繰越
+                                for k in range(len(self.naki)): #鳴き自分から見て
+                                    index = (tehaiplayer + k) % len(self.naki)
+                                    data.extend(self.naki[index])
+                                for l in range(len(self.discard)): #捨て牌自分から見て
+                                    index = (tehaiplayer + l) % len(self.discard)
+                                    data.extend(self.discard[index])
+                                for m in range(len(self.score)): #点数自分から見て
+                                    index = (tehaiplayer + m) % len(self.score)
+                                    data.append(self.score[index])
+                                data.append(self.tiles) #残り牌数
+                                data.append(0) #0が鳴きなし 1がカン
+                    else:
+                        raise ValueError("todoがおかしい")
